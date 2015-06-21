582ca8fc06a184731612e0be7a3e5a012b2ca55b
Phil Chapman <PChapman@PICR.man.ac.uk>
2015-06-21 12:42:30 +0100

    Added functionality to make toy datasets

--- R/ImportFunctions.R
+++ R/ImportFunctions.R
@@ -2,7 +2,7 @@
 
 setupSQLite <- function ( dbpath ) {
   require(RSQLite)
-  dbpath <- '~/BigData/CellLineData/CancerCellLines.db'
+  #dbpath <- '~/BigData/CellLineData/CancerCellLines.db'
   drv <- dbDriver("SQLite")
   con <- dbConnect(drv, dbname = dbpath)
   return(con)
@@ -10,7 +10,7 @@ setupSQLite <- function ( dbpath ) {
 
 importCCLE_info <- function ( fn , con )  {
 
-  fn <- '~/BigData/CellLineData/RawData/CCLE_sample_info_file_2012-10-18.txt'
+  #fn <- '~/BigData/CellLineData/RawData/CCLE_sample_info_file_2012-10-18.txt'
 
   data <- read.table(fn,header=T, sep='\t')
   colnames(data) <- c('CCLE_name', 'Primary_cell_name', 'Cell_line_aliases', 'Gender', 'Site_primary', 'Histology', 'Hist_subtype1', 'Notes', 'Source', 'Expression_arrays', 'SNP_arrays', 'Oncomap', 'Hybrid_capture_sequencing')
@@ -22,7 +22,7 @@ importCCLE_info <- function ( fn , con )  {
 
 importCCLE_affy <- function ( fn , con ) {
   require(reshape2)
-  fn <- '~/BigData/CellLineData/RawData/CCLE_Expression_Entrez_2012-09-29.gct'
+  #fn <- '~/BigData/CellLineData/RawData/CCLE_Expression_Entrez_2012-09-29.gct'
 
   data <- read.table(fn,header=T, sep='\t', skip=2)
 
@@ -43,7 +43,7 @@ importCCLE_affy <- function ( fn , con ) {
 importCCLE_hybcap <- function ( fn , con ) {
 
   require(readxl)
-  fn <- '~/BigData/CellLineData/RawData/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.xlsx'
+  #fn <- '~/BigData/CellLineData/RawData/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.xlsx'
 
   data <- read_excel(fn, col_types=c('text', 'numeric', 'text', 'numeric', 'text', 'numeric', 'numeric', 'text', 'text', 'text',
                                      'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text'))
@@ -53,10 +53,24 @@ importCCLE_hybcap <- function ( fn , con ) {
 }
 
 importCCLE_drugresponse <- function (fn , con) {
-  fn <- '~/BigData/CellLineData/RawData/CCLE_NP24.2009_Drug_data_2012.02.20.csv'
+  #fn <- '~/BigData/CellLineData/RawData/CCLE_NP24.2009_Drug_data_2012.02.20.csv'
 
   data <- read.table(fn, header=T, sep=',')
   colnames(data) <- c('CCLE_name', 'Primary_cell_name', 'Compound', 'Target', 'Doses_uM', 'Activity_median', 'Activity_sd', 'Fit_type', 'Num_data', 'EC50_uM', 'IC50_uM', 'Amax', 'Act_area')
 
   dbWriteTable(con, "ccle_drug_data", data, overwrite=TRUE)
+}
+
+makeToyDB <- function() {
+
+  toy_db <- tempfile()
+  toy_con <- setupSQLite(toy_db)
+  importCCLE_info(system.file("extdata", "CCLE_sample_info_file_2012-10-18_toy.txt", package = "CancerCellLines") , toy_con)
+  importCCLE_affy(system.file("extdata", "CCLE_Expression_Entrez_2012-09-29_toy.gct", package = "CancerCellLines") , toy_con)
+  importCCLE_hybcap(system.file("extdata", "CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07_toy.xlsx", package = "CancerCellLines") , toy_con)
+  importCCLE_drugresponse(system.file("extdata", "CCLE_NP24.2009_Drug_data_2012.02.20_toy.txt", package = "CancerCellLines") , toy_con)
+
+  print('A database of toy data has successfully been created and a connector returned')
+  return(toy_con)
+
 }
---
+++ R/ToyDataFunctions.R
@@ -0,0 +1,67 @@
+#functions to make the toy data
+
+maketoyCCLE_info <- function (fn, pattern='_BREAST', outdir=getwd()) {
+
+  fn <- '~/BigData/CellLineData/RawData/CCLE_sample_info_file_2012-10-18.txt'
+  outdir <- '~/Documents/2015 Projects/20150622 CancerCellLine Package/ToyData/'
+
+  data <- readLines(fn)
+  header <- data[1]
+  patternMatch <- data [ grepl(pattern, data)  ]
+
+  out <- c(header, patternMatch)
+  outfn <- gsub('.txt', '_toy.txt', basename(fn))
+  write( out, paste0(outdir,outfn))
+
+}
+
+maketoyCCLE_affy <- function (fn, pattern='_BREAST', outdir=getwd()) {
+
+  fn <- '~/BigData/CellLineData/RawData/CCLE_Expression_Entrez_2012-09-29.gct'
+  outdir <- '~/Documents/2015 Projects/20150622 CancerCellLine Package/ToyData/'
+
+  data <- read.table(fn,header=T, sep='\t', skip=2, stringsAsFactors = FALSE)
+  reqcols <- grepl(pattern, colnames(data))
+  reqcols[1:2] <- TRUE
+  data <- data [  , reqcols ]
+
+  header <- readLines(fn, n=2)
+
+  outfn <- gsub('.gct', '_toy.gct', basename(fn))
+  outpath <- paste0(outdir,outfn)
+  write( header, outpath)
+  write.table(data, outpath, sep='\t', append=TRUE, row.names=FALSE, quote = FALSE)
+
+}
+
+maketoyCCLE_hybcap <- function (fn, pattern='_BREAST', outdir=getwd()) {
+
+  require(readxl)
+  require(xlsx)
+  fn <- '~/BigData/CellLineData/RawData/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.xlsx'
+  outdir <- '/Users/pchapman/Documents/2015 Projects/20150622 CancerCellLine Package/ToyData/'
+
+  data <- read_excel(fn, col_types=c('text', 'numeric', 'text', 'numeric', 'text', 'numeric', 'numeric', 'text', 'text', 'text',
+                                     'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text'))
+
+  out <- data [ grepl(pattern, data$Tumor_Sample_Barcode ) , ]
+
+  outfn <- gsub('.xlsx', '_toy.xlsx', basename(fn))
+  write.xlsx( out, paste0(outdir,outfn), row.names = FALSE)
+
+}
+
+maketoyCCLE_drugresponse <- function (fn, pattern='_BREAST', outdir=getwd()) {
+
+  fn <- '~/BigData/CellLineData/RawData/CCLE_NP24.2009_Drug_data_2012.02.20.csv'
+  outdir <- '~/Documents/2015 Projects/20150622 CancerCellLine Package/ToyData/'
+
+  data <- readLines(fn)
+  header <- data[1]
+  patternMatch <- data [ grepl(pattern, data)  ]
+
+  out <- c(header, patternMatch)
+  outfn <- gsub('.csv', '_toy.txt', basename(fn))
+  write( out, paste0(outdir,outfn))
+
+}
