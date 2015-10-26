7af31ec9c16d32752e197cceede8f9b64c0e0e10
Phil Chapman <PChapman@PICR.man.ac.uk>
2015-10-26 19:17:41 +0000

    Version 0.4.1: Made sure that cell lines with no sequencing data handled properly and checked for duplicates in getAffyData and get CopyNumberdata functions

--- R/ExportFunctions.R
+++ R/ExportFunctions.R
@@ -91,31 +91,56 @@ getResponseData <- function(src, drugs, cell_lines, resp_type='ccle') {
 #' @return A \code{data.frame} containing the hybrid capture sequencing data for the requested compounds and cell lines
 #' @export
 getHybcapData <- function(con, genes, cell_lines) {
-  require(dplyr)
 
+  #make sql
   genes.sql <- paste(genes, collapse="','")
   cell_lines.sql <- paste(cell_lines, collapse="','")
   sql <- sprintf("select Tumor_Sample_Barcode as CCLE_name, Hugo_Symbol as ID,  Protein_Change, Variant_Classification
                  from ccle_hybcap
                  where CCLE_name IN ('%s') and Hugo_Symbol IN ('%s')", cell_lines.sql, genes.sql)
+
+  #get data
   data <- dbGetQuery(con, sql)
 
+  #process variant classifications - only certain types counted as variants
   data <- data %>% filter(grepl('Missense|Nonsense|Frame_Shift', Variant_Classification)) %>%
       select(-Variant_Classification) %>% group_by(CCLE_name, ID) %>%
     summarise(original=paste(Protein_Change, collapse='|'), value=1) %>% ungroup()
 
-  blank_data <- merge(data.frame(CCLE_name = cell_lines, stringsAsFactors = FALSE),
-                      data.frame(ID = genes, stringsAsFactors = FALSE))
+  #which samples were actually sequenced?
+  tested <- dbGetQuery(con, 'select distinct Tumor_Sample_Barcode as CCLE_name from ccle_hybcap')
+  tested <- tested %>% filter(CCLE_name %in% cell_lines)
+  sequenced_ids <- tested$CCLE_name
+  notsequenced_ids <- setdiff(cell_lines, sequenced_ids)
 
-  data <- blank_data %>% left_join(data, by=c('CCLE_name', 'ID')) %>%
-    mutate(original=ifelse(is.na(original), 'wt', original),
-           value = ifelse(is.na(value), 0, value),
-           Type = 'hybcap') %>%
-    select (CCLE_name, ID, Type, original, value)
+  #generate dataframes for sequenced and not sequenced cell lines
+  sequenced.df <- data.frame ( CCLE_name=rep(sequenced_ids, length(genes)) ,
+                                   ID=rep(genes, each= length(sequenced_ids) ),
+                                   original='-',
+                                   value=0, stringsAsFactors=FALSE)
 
-  data <- data %>% mutate_each(funs(as.character), -value) %>% mutate_each(funs(as.numeric), value)
+  if (length(notsequenced_ids) > 0 ) {
 
-  return(data)
+    notsequenced.df <- data.frame ( CCLE_name=rep(notsequenced_ids, length(genes)) ,
+                                        ID=rep(genes, each= length(notsequenced_ids) ),
+                                        original=NA,
+                                        value=NA, stringsAsFactors=FALSE )
+
+  } else {
+    notsequenced.df <- data.frame ()
+  }
+
+  #get rid of rows in sequenced.df which are duplicated in data
+  sequenced.df <- sequenced.df %>% filter(!( paste(CCLE_name, ID) %in% paste(data$CCLE_name, data$ID) ))
+
+  #combine hybcap dataframes and add additional standard columns
+  outdata <- bind_rows(data, sequenced.df, notsequenced.df) %>%
+    transmute(CCLE_name, ID, Type='hybcap', original, value)
+
+  #make sure data types correct
+  outdata <- outdata %>% mutate_each(funs(as.character), -value) %>% mutate_each(funs(as.numeric), value)
+
+  return(outdata)
 
 }
 
@@ -129,8 +154,8 @@ getHybcapData <- function(con, genes, cell_lines) {
 #' @return A \code{data.frame} containing the hybrid capture sequencing data for the requested compounds and cell lines
 #' @export
 getCosmicCLPData <- function(con, genes, cell_lines) {
-  require(dplyr)
 
+  #make sql - process cell line ids into unified ids
   genes.sql <- paste(genes, collapse="','")
   cell_lines.sql <- paste(cell_lines, collapse="','")
   sql <- sprintf("select t2.unified_id as CCLE_name, t1.gene_name as ID,  t1.mutation_aa, t1.mutation_description
@@ -138,25 +163,52 @@ getCosmicCLPData <- function(con, genes, cell_lines) {
                   inner join cell_line_ids t2 on t1.sample_name = t2.native_id
                  where t2.id_type = 'cosmic_clp' and
                  t2.unified_id IN ('%s') and gene_name IN ('%s')", cell_lines.sql, genes.sql)
+
+  #get data
   data <- dbGetQuery(con, sql)
 
+  #process variant classifications - only certain types counted as variants
   data <- data %>% filter(grepl('Missense|Nonsense|Frameshift', mutation_description)) %>%
     select(-mutation_description) %>% group_by(CCLE_name, ID) %>%
     summarise(original=paste(mutation_aa, collapse='|'), value=1) %>% ungroup()
 
-  blank_data <- merge(data.frame(CCLE_name = cell_lines, stringsAsFactors = FALSE),
-                      data.frame(ID = genes, stringsAsFactors = FALSE))
+  #which samples were actually sequenced?
+  tested <- dbGetQuery(con, "select distinct t2.unified_id as CCLE_name
+                       from cosmicclp_exome t1
+                       inner join cell_line_ids t2 on t1.sample_name = t2.native_id
+                       where t2.id_type = 'cosmic_clp'")
+  tested <- tested %>% filter(CCLE_name %in% cell_lines)
+  sequenced_ids <- tested$CCLE_name
+  notsequenced_ids <- setdiff(cell_lines, sequenced_ids)
 
-  data <- blank_data %>% left_join(data, by=c('CCLE_name', 'ID')) %>%
-    mutate(original=ifelse(is.na(original), 'wt', original),
-           value = ifelse(is.na(value), 0, value),
-           Type = 'cosmicclp') %>%
-    select (CCLE_name, ID, Type, original, value)
+  #generate dataframes for sequenced and not sequenced cell lines
+  sequenced.df <- data.frame ( CCLE_name=rep(sequenced_ids, length(genes)) ,
+                               ID=rep(genes, each= length(sequenced_ids) ),
+                               original='-',
+                               value=0, stringsAsFactors=FALSE)
 
-  data <- data %>% mutate_each(funs(as.character), -value) %>% mutate_each(funs(as.numeric), value)
+  if (length(notsequenced_ids) > 0 ) {
 
-  return(data)
+    notsequenced.df <- data.frame ( CCLE_name=rep(notsequenced_ids, length(genes)) ,
+                                    ID=rep(genes, each= length(notsequenced_ids) ),
+                                    original=NA,
+                                    value=NA, stringsAsFactors=FALSE )
 
+  } else {
+    notsequenced.df <- data.frame ()
+  }
+
+  #get rid of rows in sequenced.df which are duplicated in data
+  sequenced.df <- sequenced.df %>% filter(!( paste(CCLE_name, ID) %in% paste(data$CCLE_name, data$ID) ))
+
+  #combine hybcap dataframes and add additional standard columns
+  outdata <- bind_rows(data, sequenced.df, notsequenced.df) %>%
+    transmute(CCLE_name, ID, Type='cosmicclp', original, value)
+
+  #make sure data types correct
+  outdata <- outdata %>% mutate_each(funs(as.character), -value) %>% mutate_each(funs(as.numeric), value)
+
+  return(outdata)
 
 }
 
@@ -178,6 +230,10 @@ getAffyData <- function(con, genes, cell_lines) {
                where CCLE_name IN ('%s') and Symbol IN ('%s')", cell_lines.sql, genes.sql)
 
   data <- dbGetQuery(con, sql)
+
+  #duplicate remove
+  data <- data %>% group_by(ID) %>% mutate(N=n()) %>% ungroup %>% filter(N==median(N)) %>% select(-N) %>% as.data.frame
+
   data <- data %>% mutate_each(funs(as.character), -value) %>% mutate_each(funs(as.numeric), value)
 
   return(data)
@@ -202,6 +258,10 @@ getCopyNumberData <- function(con, genes, cell_lines) {
                where CCLE_name IN ('%s') and Symbol IN ('%s')", cell_lines.sql, genes.sql)
 
   data <- dbGetQuery(con, sql)
+
+  #duplicate remove
+  data <- data %>% group_by(ID) %>% mutate(N=n()) %>% ungroup %>% filter(N==median(N)) %>% select(-N) %>% as.data.frame
+
   data <- data %>% mutate_each(funs(as.character), -value) %>% mutate_each(funs(as.numeric), value)
 
   return(data)
