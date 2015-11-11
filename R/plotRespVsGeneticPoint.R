fa17aa5ba9e30230780304d48d372f846ced2f78
Phil Chapman <PChapman@PICR.man.ac.uk>
2015-11-11 21:18:31 +0000

    Separated out RespVsGenetic plots into separate functions and updated the visualisations vignette

---
+++ R/plotRespVsGeneticPoint.R
@@ -0,0 +1,41 @@
+#' Compare response against a genetic feature (scatter/boxplot)
+#'
+#' Generate a plot that compares response in cell lines with different genetic features
+#'
+#' @param df A data frame from the \code{makeRespVsGeneticDataFrame} function
+#' @return A ggplot2 object
+#' @export
+plotRespVsGeneticPoint <- function(df, data_type=NULL, facet_option=FALSE) {
+
+  plot_data <- df %>% arrange(desc(resp_value))
+
+  if(data_type %in% c('hybcap', 'cosmicclp')) {
+    p <- ggplot(plot_data, aes(x=as.factor(feature_value), y=resp_value) ) +
+      geom_boxplot(outlier.size=0, aes(colour=as.factor(feature_value))) +
+      geom_point(aes(fill=as.factor(feature_value)), shape=21, size=rel(2), position = position_jitter(width=0.1)) +
+      xlab(unique(plot_data$feature_name)) + ylab(unique(plot_data$resp_id)) +
+      theme_bw()
+
+    if (facet_option) {
+      p <- p + facet_wrap(~tissue)
+    }
+
+    return(p)
+
+  } else {
+    p <- ggplot(plot_data, aes(x=feature_value, y=resp_value) ) +
+      geom_point(aes(fill=scale(feature_value)), shape=21, size=rel(2)) +
+      stat_smooth(method = 'lm') +
+      scale_fill_gradient2(low='blue', mid='white', high='red') +
+      xlab(unique(plot_data$feature_name)) + ylab(unique(plot_data$resp_id)) +
+      theme_bw() + theme(legend.position='none')
+
+    if (facet_option) {
+      p <- p + facet_wrap(~tissue)
+    }
+    return(p)
+
+  }
+
+}
+
--- R/shinyRespVsGeneticServer.R
+++ R/shinyRespVsGeneticServer.R
@@ -98,77 +98,13 @@ shinyRespVsGeneticServer <- function(input, output, con, drug_df=NULL) {
 
   output$plot1 <- renderPlot({
 
-    plot_data <- proc_data() %>% arrange(desc(resp_value))
-
-    if(input$data_type %in% c('hybcap', 'cosmicclp')) {
-      p <- ggplot(plot_data, aes(x=as.factor(feature_value), y=resp_value) ) +
-        geom_boxplot(outlier.size=0, aes(colour=as.factor(feature_value))) +
-        geom_point(aes(fill=as.factor(feature_value)), shape=21, size=rel(2), position = position_jitter(width=0.1)) +
-        xlab(unique(plot_data$feature_name)) + ylab(unique(plot_data$resp_id)) +
-        theme_bw()
-
-      if (input$facet_option) {
-        p <- p + facet_wrap(~tissue)
-      }
-
-      return(p)
-
-    } else {
-      p <- ggplot(plot_data, aes(x=feature_value, y=resp_value) ) +
-        geom_point(aes(fill=scale(feature_value)), shape=21, size=rel(2)) +
-        stat_smooth(method = 'lm') +
-        scale_fill_gradient2(low='blue', mid='white', high='red') +
-        xlab(unique(plot_data$feature_name)) + ylab(unique(plot_data$resp_id)) +
-        theme_bw() + theme(legend.position='none')
-
-      if (input$facet_option) {
-        p <- p + facet_wrap(~tissue)
-      }
-      return(p)
-
-    }
+    plotRespVsGeneticPoint(proc_data(), data_type=input$data_type, facet_option=input$facet_option)
 
   })
 
   output$plot2 <- renderPlot({
 
-    plot_data <- proc_data() %>% filter(!is.na(resp_value)) %>% arrange(desc(resp_value))
-
-    if(input$data_type %in% c('hybcap', 'cosmicclp')) {
-      p <- ggplot(plot_data, aes(x=CCLE_name, y=resp_value, fill=as.factor(feature_value)) ) +
-        geom_bar(stat='identity') +
-        scale_x_discrete(limits=plot_data$CCLE_name) +
-        ylab(unique(plot_data$resp_id)) +
-        theme_bw() + theme(axis.text.x = element_text(size=rel(1), angle=330, hjust=0, vjust=1))
-
-      if(nrow(plot_data) > 30 | input$facet_option) {
-        p <- p + theme(axis.text.x=element_text(size=0))  #remove cell line id's if more than 30 cell lines
-      }
-
-      if (input$facet_option) {
-        p <- p + facet_wrap(~tissue)
-      }
-
-      return(p)
-
-    } else {
-      p <- ggplot(plot_data, aes(x=CCLE_name, y=resp_value, fill=scale(feature_value)) ) +
-        geom_bar(stat='identity') +
-        scale_x_discrete(limits=plot_data$CCLE_name) +
-        scale_fill_gradient2(low='blue', mid='white', high='red') +
-        ylab(unique(plot_data$resp_id)) +
-        theme_bw() + theme(legend.position='none', axis.text.x = element_text(size=rel(1), angle=330, hjust=0, vjust=1))
-
-      if(nrow(plot_data) > 30 | input$facet_option) {
-        p <- p + theme(axis.text.x=element_text(size=0))  #remove cell line id's if more than 30 cell lines
-      }
-
-      if (input$facet_option) {
-        p <- p + facet_wrap(~tissue)
-      }
-
-      return(p)
-    }
+    plotRespVsGeneticHist(proc_data(), input$data_type, input$facet_option)
 
   })
 
